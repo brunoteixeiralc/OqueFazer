b21f8ab3c31f959197cbdba5e5213ca0ab345032
bestlook <bestlookbl@gmail.com>
2015-05-08 17:01:25 -0300

    Initial Commit

---
+++ OQueFazer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OQueFazer
+//
+//  Created by Bruno Corrêa on 08/05/15.
+//  Copyright (c) 2015 Bruno Corrêa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
